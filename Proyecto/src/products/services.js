278095c7432507f39d42b4a056e8e44bbfdf8e31
Lukaina <veronica.alcaraz@mercadolibre.com.co>
2021-11-23 15:29:44 -0500

    Products module - Services

---
+++ Proyecto/src/products/services.js
@@ -0,0 +1,27 @@
+const { ObjectId } = require('mongodb');
+//Traer módulo de la base de datos
+const { Database } = require('../database/index');
+
+const COLLECTION = 'products';
+
+const getAll = async () => { //Traerá todos los productos
+    const collection = await Database(COLLECTION);
+    return await collection.find({}).toArray();
+}
+
+const getById = async (id) => {
+    const collection = await Database(COLLECTION);
+    return collection.findOne({ _id: ObjectId(id) });
+}
+
+const create = async (product) => {
+    const collection = await Database(COLLECTION);
+    let result = collection.insertOne(product);
+    return result.insertedId
+}
+
+module.exports.ProductsService = {
+    getAll,
+    getById,
+    create
+}
